ee7c090c31166e61c87e49f9b19b4775059c4935
Hadil Alabrash <hadeelalabrash43@gmail.com>
2022-04-22 12:20:23 +0200

    updates

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import React, {useEffect, useState, useRef} from "react";
+
+function App() {
+	const [memes, setMemes] = useState([]);
+	const canvasRef = useRef(null);
+	const ref = useRef(null);
+	useEffect(() => {
+		const canvas = canvasRef.current;
+
+		const context = canvas.getContext("2d");
+		const api = "https://api.imgflip.com/get_memes";
+		const fetchData = async () => {
+			try {
+				const response = await fetch(api);
+				const json = await response.json();
+
+				setMemes(json.data.memes);
+				json.data.memes.map((image) => {
+					var imageObj = new Image();
+					imageObj.src = image.url;
+					imageObj.onload = function () {
+						context.drawImage(imageObj, 15, 20);
+					};
+					return imageObj;
+				});
+			} catch (error) {
+				console.log("error", error);
+			}
+		};
+		fetchData();
+	}, []);
+
+	return (
+		<div className="container">
+			<canvas
+				className="canvas"
+				width={1920}
+				height={1080}
+				ref={canvasRef}
+			></canvas>
+		</div>
+	);
+}
+
+export default App;
